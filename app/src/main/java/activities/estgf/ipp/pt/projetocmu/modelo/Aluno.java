2bde156c1c3f56bdcd3ccb9b950a996908df8381
Tamires <crisoliveira.93@gmail.com>
2018-01-12 21:21:47 +0000

    criacao da classe Aluno

--- app/src/main/java/activities/estgf/ipp/pt/projetocmu/modelo/Aluno.java
+++ app/src/main/java/activities/estgf/ipp/pt/projetocmu/modelo/Aluno.java
@@ -3,28 +3,20 @@ package activities.estgf.ipp.pt.projetocmu.modelo;
 import java.io.Serializable;
 
 public class Aluno implements Serializable{
-    private long id;
+    private long idAluno;
     private String nome;
-    private String curriculo;
     private String email;
-    private String telefone;
-    private String cursos;
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
+    public long getIdAluno() {return idAluno;}
+    public void setIdAluno(long idAluno){this.idAluno = idAluno;}
 
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
 }
