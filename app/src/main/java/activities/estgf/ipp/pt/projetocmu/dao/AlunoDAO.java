77009d87414eb0156f29ed8ed088473ea7628c5b
Tamires <crisoliveira.93@gmail.com>
2018-01-13 17:44:20 +0000

    criacao da classe Aluno

--- app/src/main/java/activities/estgf/ipp/pt/projetocmu/dao/AlunoDAO.java
+++ app/src/main/java/activities/estgf/ipp/pt/projetocmu/dao/AlunoDAO.java
@@ -1,6 +1,26 @@
 package activities.estgf.ipp.pt.projetocmu.dao;
 
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import activities.estgf.ipp.pt.projetocmu.modelo.Aluno;
+
 public class AlunoDAO {
+    private Context contexto;
     private HelperDAO dao;
+
+    public AlunoDAO(Context contexto) {this.contexto = contexto;}
+
+    public void insereAluno(Aluno aluno){
+        dao = new HelperDAO(contexto);
+        ContentValues values = new ContentValues();
+        values.put("nome",aluno.getNome());
+        values.put("email",aluno.getNome());
+
+        dao.getWritableDatabase().insert("Aluno",null,values);
+        dao.close();
+    }
+
 }
